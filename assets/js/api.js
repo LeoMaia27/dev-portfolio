31064b6152aaf00fa7f42752ebaa0de4237b1bf2
Leonardo <you@example.com>
2024-01-27 09:15:37 -0300

    adicionando informações ao json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/RenanJPaula/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/LeoMaia27/portfolio/main/data/profile.json';
     const fetching = await fetch(url);
     console.log(fetching.status === 200 ? true : false);
 
